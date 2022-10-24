664ea42333abf06a7fa9de017a9f20687b446981
HuyNet <quanghuy10102001@outlook.com>
2022-10-24 22:32:30 +0700

    Bug dong 76 ThangFragment.java

--- app/src/main/java/com/example/lichvannien/ThangFragment.java
+++ app/src/main/java/com/example/lichvannien/ThangFragment.java
@@ -1,13 +1,20 @@
 package com.example.lichvannien;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +27,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class ThangFragment extends Fragment {
     ImageView NextButton,PreviousButton;
@@ -59,10 +67,60 @@ public class ThangFragment extends Fragment {
                 SetUpCalendar();
             }
         });
+        girdView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder builder= new AlertDialog.Builder(context);
+                builder.setCancelable(true);
+                View addView =LayoutInflater.from(adapterView.getContext()).inflate(R.layout.add_event_layout,null);
+                EditText EventName = addView.findViewById(R.id.event_id);
+                TextView EventTime = addView.findViewById(R.id.event_time);
+                ImageView SetTime= addView.findViewById(R.id.set_event_time);
+                Button AddEvent=addView.findViewById(R.id.add_event);
+                SetTime.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Calendar calendar=Calendar.getInstance();
+                        int hours =calendar.get(Calendar.HOUR_OF_DAY);
+                        int minuts = calendar.get(Calendar.MINUTE);
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(addView.getContext(), androidx.appcompat.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                Calendar c = Calendar.getInstance();
+                                c.set(Calendar.HOUR_OF_DAY,i);
+                                c.set(Calendar.MINUTE,i1);
+                                c.setTimeZone(TimeZone.getDefault());
+                                SimpleDateFormat hformate = new SimpleDateFormat("K:mm a",vn);
+                                String event_time = hformate.format(c.getTime());
+                                EventTime.setText(event_time);
+                            }
+                        },hours,minuts,false);
+                    }
+                });
+
+                AddEvent.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                     SaveE
+                    }
+                });
+            }
+        });
         return view;
     }
+
     private void SetUpCalendar(){
         String currentDate = dateFormat.format(calendar.getTime());
         CurrentDate.setText(currentDate);
+        dates.clear();
+        Calendar monthCalendar =(Calendar) calendar.clone();
+        monthCalendar.set(Calendar.DAY_OF_MONTH,1);
+        int FirstDayofMonth = monthCalendar.get(Calendar.DAY_OF_WEEK)-1;
+        monthCalendar.add(Calendar.DAY_OF_MONTH,-FirstDayofMonth);
+
+        while (dates.size()<MAX_CELANDAR_DAYS){
+            dates.add(monthCalendar.getTime());
+            monthCalendar.add(Calendar.DAY_OF_MONTH,1);
+        }
     }
 }
