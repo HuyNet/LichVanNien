723d77f73fa7bae0b014242121de6201b45abbcd
HuyNet <quanghuy10102001@outlook.com>
2022-10-24 20:06:21 +0700

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/lichvannien/DBOpenHelper.java
+++ app/src/main/java/com/example/lichvannien/DBOpenHelper.java
@@ -9,14 +9,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DBOpenHelper extends SQLiteOpenHelper {
-    private final static  String CREATE_EVENTS_TABLE = "create table"
-            +DBStructure.EVENT_TABLE_NAME+"(ID INTERGER PRIMARY KEY AUTOINCREMENT)"
-            +DBStructure.EVENT+"TEXT, "
-            +DBStructure.TIME+"TEXT, "
-            +DBStructure.DATE+"TEXT, "
-            +DBStructure.MONTH+"TEXT,"
-            +DBStructure.YEAR+"TEXT, ";
-    private static final String DROP_EVENTS_TABLE="DROP TABLE IF EXIST "+DBStructure.EVENT_TABLE_NAME;
+    private final static  String CREATE_EVENTS_TABLE = "create table " +DBStructure.EVENT_TABLE_NAME+" (ID INTERGER PRIMARY KEY AUTOINCREMENT,"
+            +DBStructure.EVENT+" TEXT, "+DBStructure.TIME+" TEXT, " +DBStructure.DATE+" TEXT, " +DBStructure.MONTH+" TEXT, "
+            +DBStructure.YEAR+" TEXT )";
+    private static final String DROP_EVENTS_TABLE="DROP TABLE IF EXISTS "+DBStructure.EVENT_TABLE_NAME;
     public DBOpenHelper(@Nullable Context context) {
         super(context, DBStructure.DB_NAME, null, DBStructure.DB_VERSION);
     }
